3b14a01cf9d0c0697d299bdc9188b601861c9639
john <johnadams@mail.usf.edu>
2019-03-27 19:21:49 -0400

    main now calls benchmarkrunner

--- src/main.py
+++ src/main.py
@@ -3,12 +3,6 @@ from benchmarkrunner import benchmarkrunner
 
 def main():
     benchmarkrunner()
-    #t4_21 = read_Netlist('t4_21.ckt')
-    #t4_3 = read_Netlist('t4_3.ckt')
-    #t5_10 = read_Netlist('t5_10.ckt')
-    #t5_26a = read_Netlist('t5_26a.ckt')
-    #t6_24 = read_Netlist('t6_24.ckt')
-
-
+  
 if __name__ == '__main__':
     main()
