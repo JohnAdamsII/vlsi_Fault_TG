96479e35061b8dc8aa00dc42514e7a1625faef36
johnadamsii <johnadams@mail.usf.edu>
2019-03-30 17:37:17 -0400

    added TG algorithm info from book to resources

--- src/main.py
+++ src/main.py
@@ -3,10 +3,9 @@ from benchmarkrunner import benchmarkrunner,printCircuit
 
 def main():
     ckt_file = "t4_21.ckt"
-    ckt = read_Netlist(ckt_file)  #use to just one circuits data
+    ckt = read_Netlist(ckt_file)  #use to read one circuits data
     printCircuit(ckt,ckt_file)
 
     #benchmarkrunner()      #use to print data of all benchmark circuits
-  
 if __name__ == '__main__':
     main()
